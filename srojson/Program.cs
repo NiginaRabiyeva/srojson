d78983f0d5f3ae34cc2965a2f00668b711d572f1
Nigina <nigina06.20@gmail.com>
2025-02-24 11:03:28 +0500

    добавляем swagger

--- srojson/Program.cs
+++ srojson/Program.cs
@@ -1,17 +1,27 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models; // <== ????????? ??? Swagger
+using Microsoft.OpenApi.Models; // <== Добавляем для Swagger
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
+
 app.MapGet("/hello", () => new { message = "Hello, World!" });
+
 app.Run();
