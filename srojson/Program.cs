e23d4fdec596138371714af7de6372f2433bdaae
Nigina <nigina06.20@gmail.com>
2025-02-24 10:57:24 +0500

    добавляю using

--- srojson/Program.cs
+++ srojson/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models; // <== ????????? ??? Swagger
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapGet("/hello", () => new { message = "Hello, World!" });
 app.Run();
